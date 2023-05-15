692d1b18d0a1d9ff1d1bfa2ed3c4b07d8001db3d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-15 14:16:13 +0530

    Update MainActivity.java

--- labProgramFive/MainActivity.java
+++ labProgramFive/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//---------initialisation--------//
 public class MainActivity extends AppCompatActivity {
     private int counter=0;
     private boolean running;
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if(savedInstanceState!=null){
             counter=savedInstanceState.getInt("counter");
             running=savedInstanceState.getBoolean("running");
@@ -23,15 +22,14 @@ public class MainActivity extends AppCompatActivity {
         runTimer();
 
     }
-
+    //-----------Main Timer------------//
     private void runTimer() {
         final Handler handler=new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-
-                    final TextView count=findViewById(R.id.textView);
-                    count.setText("Count : " + counter);
+                final TextView count=findViewById(R.id.textView);
+                count.setText("Count : " + counter);
                 if(running) {
                     counter++;
                 }
@@ -39,17 +37,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //-------start button--------//
     public void startTimer(View view) {
         Toast.makeText(this, "Started", Toast.LENGTH_SHORT).show();
         running=true;
     }
-
+    //-----stop button---------//
     public void stopTimer(View view) {
         Toast.makeText(this, "Paused", Toast.LENGTH_SHORT).show();
         running=false;
     }
-
+    //-----reset button------//
     public void reSetTimer(View view) {
         Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
         running=false;
